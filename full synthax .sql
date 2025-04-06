07a2e3e283596180b0dccb9b34aa1e30e5558473
Ishimwe Moses <163633528+ishimweMOSES@users.noreply.github.com>
2025-04-06 16:58:04 +0200

    Update full synthax .sql

--- full synthax .sql
+++ full synthax .sql
@@ -80,3 +80,13 @@ WITH first_emps AS (
 )
 SELECT * FROM first_emps
 WHERE row_num <= 2;
+
+
+-- aggregation with windows function 
+
+SELECT 
+  emp_id, emp_name, department, salary,
+  MAX(salary) OVER (PARTITION BY department) AS dept_max_salary,
+  MAX(salary) OVER () AS overall_max_salary
+FROM employees;
+
