bb6c7371db2fb64457996bce16d168a42ade1522
Karenzi Audace <karenaudace@gmail.com>
2025-04-06 16:54:23 +0200

    Update full synthax .sql

--- full synthax .sql
+++ full synthax .sql
@@ -63,3 +63,20 @@ WITH ranked_emps AS (
 SELECT * FROM ranked_emps
 WHERE rank_position <= 3;
 
+
+
+
+--first 2 employees per department
+
+WITH first_emps AS (
+  SELECT emp_id,
+         emp_name,
+         department,
+         region,
+         salary,
+         hire_date,
+         ROW_NUMBER() OVER (PARTITION BY department ORDER BY hire_date) AS row_num
+  FROM employees
+)
+SELECT * FROM first_emps
+WHERE row_num <= 2;
