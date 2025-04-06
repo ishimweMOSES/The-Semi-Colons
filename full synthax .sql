bbd4a7fe1cb3445ac016c275e968978004e73642
Ishimwe Moses <163633528+ishimweMOSES@users.noreply.github.com>
2025-04-06 16:23:21 +0200

    Update full synthax .sql

--- full synthax .sql
+++ full synthax .sql
@@ -45,3 +45,21 @@ SELECT
   RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS rank_standard,
   DENSE_RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS rank_dense
 FROM employees;
+
+
+
+-- synthax for top 3 highest salaries per sales department 
+
+WITH ranked_emps AS (
+  SELECT emp_id,
+         emp_name,
+         department,
+         region,
+         salary,
+         hire_date,
+         RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS rank_position
+  FROM employees
+)
+SELECT * FROM ranked_emps
+WHERE rank_position <= 3;
+
